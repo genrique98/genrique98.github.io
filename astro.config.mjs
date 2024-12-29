29eb108b98f9ae17d50f1dae657dcf521047c603
genrique98 <15620759+genrique98@users.noreply.github.com>
2024-12-28 16:13:02 -0800

    gh-pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://genrique98.github.io',
+	base: 'genrique98.github.io',
 	integrations: [mdx(), sitemap()],
-});
+  })
